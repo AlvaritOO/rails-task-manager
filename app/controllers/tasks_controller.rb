67bc4f657f5daf3d157929c806d653c20ecb419d
ÁLVARO MEDINA <alvaro.e.medina.a@gmail.com>
2021-08-05 15:49:10 -0400

    add controller

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,27 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show; end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new
+    # (task_params)
+    @task.save
+  end
+
+  def edit; end
+
+  def update
+    @task.update(task_params)
+  end
+
+  def destroy
+    @task.destroy
+  end
+end
