abad0e36924c81e4914b25aa3a882c703f090b26
ÁLVARO MEDINA <alvaro.e.medina.a@gmail.com>
2021-08-05 20:44:52 -0400

    add views

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,19 +3,22 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
-  def show; end
+  def show
+    @task = Task.find(params[:id])
+  end
 
   def new
     @task = Task.new
   end
 
   def create
-    @task = Task.new
-    # (task_params)
+    @task = Task.new(task_params)
     @task.save
   end
 
-  def edit; end
+  def edit
+    @task = Task.find(params[:id])
+  end
 
   def update
     @task.update(task_params)
